e40b660d36dfff7d52d9706b018201f4459b9f9d
Zeynab Sobhani <zeynab.sobhani6294@gmail.com>
2021-04-19 17:21:25 +0430

    Loadbalance fixed

--- AppConsumer/src/main/java/com/app2/app2/Client.java
+++ AppConsumer/src/main/java/com/app2/app2/Client.java
@@ -14,6 +14,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import  org.springframework.stereotype.Service;
 
 public class Client {
 
